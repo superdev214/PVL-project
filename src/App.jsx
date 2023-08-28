568373576229183ff58c2ac6c55b658d2339d964
superdev214 <superdev0241002@gmail.com>
2023-08-27 22:29:12 -0700

    feat: add tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-5xl text-red-800'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
